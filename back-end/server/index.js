29eea28bb5d03385957dfcb6ce19165a004bd3db
Jared Tanner <Deraj21@gmail.com>
2021-06-19 14:26:45 -0700

    starting work on server

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -1,9 +1,28 @@
 const envConfig = require("dotenv").config()
 const express = require("express")
+const { Pool, Client } = require('pg')
 const Ably = require("ably")
 const app = express()
 
 const ABLY_API_KEY = process.env.ABLY_API_KEY
 
 ////////////
+
+let { CONNECTION_STRING, HOST, DATABASE, USER, PORT, PASSWORD } = process.env
+
+console.log(process.env)
+
+const client = new Client({
+  connectionString: CONNECTION_STRING //,
+  // user: USER,
+  // host: HOST,
+  // database: DATABASE,
+  // password: PASSWORD,
+  // port: PORT
+})
+
+client.connect()
+  .then(res => console.log('db response:', res))
+  .catch(err => console.log('db error:', err))
+
 
--- front-end/src/components/LandingPage.js
+++ front-end/src/components/LandingPage.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 // components
 import LandingButtons from './LandingButtons'
